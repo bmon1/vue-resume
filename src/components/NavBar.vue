b9e0a2b8c7f3c50044b22ad4f30b340523382bd2
brand <bmm243@txstate.edu>
2023-08-24 12:00:25 -0500

    Navabr made

---
+++ src/components/NavBar.vue
@@ -0,0 +1,34 @@
+<template>
+   <v-app-bar app color="black" dark flat class="px-10">
+      <v-row justify="space-between">
+         <div class="w-1/3">
+            <v-btn >Branden</v-btn>
+         </div>
+         <v-btn text @click="scroll('home')" class="text-green">Home</v-btn>
+         <v-btn text @click="scroll('about')" class="text-green">About</v-btn>
+         <v-btn text @click="scroll('education')" class="text-green">Education</v-btn>
+         <v-btn text @click="scroll('projects')" class="text-green">Projects</v-btn>
+         <v-btn text @click="scroll('work_experience')" class="text-green">Work Experience</v-btn>
+         <v-btn text @click="scroll('resume')" class="text-green">Resume</v-btn>
+         <v-btn text @click="scroll('contact')" class="text-green">Contact Me</v-btn>
+      </v-row>
+   </v-app-bar>
+   
+</template>
+
+
+<script>
+export default {
+   methods: {
+      scroll(refName) {
+         const element = document.getElementById(refName);
+         element.scrollIntoView({behavior: "smooth"});
+      }
+   }
+}
+</script>
+
+
+<style>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<template>
+   <v-app id="home">
+      <NavBar />
+      <v-container fluid>
+
+      </v-container>
+   </v-app>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+import NavBar from '@/components/NavBar.vue'
+
+export default defineComponent ({
+   name: 'HomeView',
+   components: {
+      NavBar,
+   },
+});
+</script>
