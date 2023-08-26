20cf8b447054bae4d5a53dc0d3a9aea48c80110b
brand <bmm243@txstate.edu>
2023-08-26 12:45:32 -0500

    Created intro

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,19 @@
       <NavBar />
       <v-container fluid>
          <div class="head">
+            <v-row>
+               <v-col cols="5">
+                  <div class="intro mt-16" style="position: relative">
+                     <h1 class="text-green">Hello,</h1>
+                     <h1 class="text-white">I'm Branden Monroe</h1>
+                     <h2 class="text-white">Software Developer with QA Experience</h2>
+                     <v-btn tile dark class="text-green mt-8" variant="outlined">Contact Me</v-btn>
+                  </div>
+               </v-col>
+               <v-col cols="7">
 
+               </v-col>
+            </v-row>
          </div>
       </v-container>
    </v-app>
